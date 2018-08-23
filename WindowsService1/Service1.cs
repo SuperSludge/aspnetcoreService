a4fbb16aa5f07bc5eb03bb42fdb3d56f2fa2889c
Neyer Mathias <neyerm@gantner.com>
2018-08-23 17:17:52 +0200

    Add project files.

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread.Sleep(10000);
+            WebApplication3.Program.BuildWebHost(args).StartAsync();
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
